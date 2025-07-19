e74fda078d910f146450ad9e85232ce72cfa9af9
cartewane <cartewane@gmail.com>
2025-07-19 19:21:23 +0700

    Update site.js

--- src/utils/site.js
+++ src/utils/site.js
@@ -3,7 +3,7 @@ export const desk = 'Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bo
 export const url = 'https://bokepberjamaah.pages.dev';
 export const terbit = '2025-07-19T01:01:03Z';
 
-export const google = 'h19EcPvR5CuiEyeT6v5Blc-wxJKv0HWqBzIuXkspxWM';
+export const google = 'c4UjlNf9Pgo0epKRg37mB7kcdbzDa2sDgSDWAafQhe0';
 export const yandex = '40f1bc43721057e3'
 
 export const index = 'e4cc26bf-1f20-4040-9ad0-26f90a47c3aa';
